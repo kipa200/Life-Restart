ad355f588c2b0bd736197fb3d6c25ae44be79679
KIPA200 <498319699@qq.com>
2021-07-28 17:19:03 +0800

    函数修改

--- animate1.js
+++ animate1.js
@@ -1,13 +1,16 @@
 // alert('已调用函数')
 function begin() {
     const element = document.getElementById('p2');
-    let start;
+    let start, startDeg;
     var timestamp = Date.parse(new Date());
+    // 通过随机数引入结束时间
+    var endtime = 3600 + 1000 * Math.random();
+    // console.log(endtime)
     function step(timestamp) {
+        // var declaration = document.styleSheets[0].cssRules[0].style;
+        // startDeg = declaration.getPropertyValue('rotate'); // 
+        // console.log(startDeg);
 
-        // 通过随机数引入结束时间
-        var endtime = 3600 + 1000 * Math.random();
-        // const startDeg = element.style.transform.rotate();
         if (start === undefined)
             start = timestamp;
         const elapsed = timestamp - start;
@@ -18,9 +21,9 @@ function begin() {
         if (elapsed < endtime) { // 在指定时间后停止动画
             window.requestAnimationFrame(step);
         }
-
+        // return startDeg
     }
-
+    // element.style.transform = 'rotate(' + startDeg + 'deg)';
     window.requestAnimationFrame(step);
 }
 
