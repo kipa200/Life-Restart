beae66d48f3af52f67ac832c7f53dc5e1241ccfa
KIPA200 <498319699@qq.com>
2021-07-28 15:38:31 +0800

    更新了动画的运行方式
    不在需要刷新网页

--- animate1.js
+++ animate1.js
@@ -1,22 +1,26 @@
-
 // alert('已调用函数')
-const element = document.getElementById('p2');
-let start;
-var timestamp = Date.parse(new Date());
-// 通过随机数引入结束时间
-var endtime = 3000 + 1000 * Math.random();
-function step(timestamp) {
-    if (start === undefined)
-        start = timestamp;
-    const elapsed = timestamp - start;
+function begin() {
+    const element = document.getElementById('p2');
+    let start;
+    var timestamp = Date.parse(new Date());
+    function step(timestamp) {
 
-    //设置转速及转动角度
-    element.style.transform = 'rotate(' + 0.1 * elapsed+ 'deg)';
+        // 通过随机数引入结束时间
+        var endtime = 3600 + 1000 * Math.random();
+        // const startDeg = element.style.transform.rotate();
+        if (start === undefined)
+            start = timestamp;
+        const elapsed = timestamp - start;
 
-    if (elapsed < endtime) { // 在指定时间后停止动画
-        window.requestAnimationFrame(step);
+        //设置转速及转动角度
+        element.style.transform = 'rotate(' + 0.1 * elapsed + 'deg)';
+
+        if (elapsed < endtime) { // 在指定时间后停止动画
+            window.requestAnimationFrame(step);
+        }
+
     }
 
+    window.requestAnimationFrame(step);
 }
 
-window.requestAnimationFrame(step);
